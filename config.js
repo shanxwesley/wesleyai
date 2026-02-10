37ac031c1f53d78a5c7e3a6b082b3d61e15dd942
shanxwesley <swbachain@gmail.com>
2026-02-10 20:28:29 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// API Configuration
-// This file should NOT be committed to GitHub
-const CONFIG = {
-    OPENROUTER_API_KEY: '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'
-};
